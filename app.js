95c05174fa31b759b3c19886a3fd2e4f461bb017
diego vallejo <vallejodiego@live.com>
2022-05-09 19:09:25 -0300

    html register y stylos

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const app = express();
 // variables barra
 const raiz = '/';
 const home = '/home';
+const register = '/register'
 
 // variables path
 const toHome = path.join(__dirname, './views/home.html');
 const publicPath = path.join(__dirname, '/public/');
-
+const toregister = path.join(__dirname, './views/register.html')
 // app.metodos
 app.listen(3000, () => { console.log('Servidor corriendo')});
 
@@ -23,4 +24,7 @@ app.get(raiz, (req, res)=>{
 });
 app.get(home, (req, res)=>{
     res.sendFile(toHome);
+});
+app.get(register, (req, res)=>{
+    res.sendFile(toregister);
 });
